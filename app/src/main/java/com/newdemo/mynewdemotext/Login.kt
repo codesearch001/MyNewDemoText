f4e3a029ca8c06b20ac009c8060c74d2d28705c4
Mohan <praveensingh2824@gmail.com>
2022-11-13 17:30:21 +0530

    Add Login File

---
+++ app/src/main/java/com/newdemo/mynewdemotext/Login.kt
@@ -0,0 +1,17 @@
+package com.newdemo.mynewdemotext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            /*kaushal sfsds sdfsdfsd */
+            /*kaushal sfsds sdfsdfsd  HELOOOOO */
+
+            /*kaushal sfsds sdfsdfsd  FFFFFFFFFF */
+
+    }
+}
